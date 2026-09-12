9702f1695fdef8728d6c2e7562355e79d9e9b1db
Shaik-Athik <shaikathik292007@gmail.com>
2026-09-12 11:28:44 +0530

    Create Arithmetic Operations.py

---
+++ Arithmetic Operations.py
@@ -0,0 +1,13 @@
+import math
+x=int(input())
+y=int(input())
+s=x+y
+d=x-y
+p=x*y
+q=x//y
+r=x%y
+print("Sum:"+str(s))
+print("Difference:"+str(d))
+print("Product:"+str(p))
+print("Quotient:"+str(q))
+print("Remainder:"+str(r))
